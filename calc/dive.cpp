4de4c5479ac68cebc411f1a9cc32a59e0d84a0e9
choi1418 <choitaeyoung04@gmail.com>
2026-06-18 14:01:59 +0900

    file dive.cpp

--- calc/dive.cpp
+++ calc/dive.cpp
@@ -1,6 +1,18 @@
-#include "dive.h"
+
+#include "sub.h"
 
 int dive(int x, int y)
 {
-	return 0;
+    int cnt = 0;
+
+    // y가 0이거나 음수일 때 무한 루프에 빠지는 것을 방지하는 예외 처리 (필요시)
+    if (y <= 0) return 0;
+
+    while (x >= y) // 정상 동작을 위해 x > y를 x >= y로 수정하는 것이 일반적입니다. (예: 4 / 2 = 2)
+    {
+        cnt++;
+        x = sub(x, y); // sub.h에 정의된 뺄셈 함수 호출
+    }
+
+    return cnt;
 }
